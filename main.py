fb26de5824dc1ee7f920538ef9cfc96b21578a63
hasira09 <88621706+hasira09@users.noreply.github.com>
2022-11-15 02:19:18 +0530

    TextField

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.screen import Screen
+
+username_helper = """
+MDTextField:
+    hint_text: "Enter Username"
+    helper_text: "or Click on Forgot Username"
+    helper_text_mode: "on_focus"
+    icon_right: "android"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+    size_hint_x: None
+    width: 300
+"""
 
 
 class BasicApp(MDApp):
 
     def build(self):
-        self.theme_cls.primary_palette = "DeepOrange"
-        self.theme_cls.primary_hue = "A700"
-        self.theme_cls.theme_style = "Dark"
         screen = Screen()
-        btn_f = MDRectangleFlatButton(text='Hello World',
-                                      pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        self.theme_cls.primary_palette = "Green"
+        # username = MDTextField(text='Enter Username',
+        #                        pos_hint={'center_x': 0.5, 'center_y': 0.5},
+        #                        size_hint_x=None, width=300)
 
-        screen.add_widget(btn_f)
+        username = Builder.load_string(username_helper)
+        screen.add_widget(username)
         return screen
 
 
