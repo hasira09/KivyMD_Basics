796cfb3e4ba5f94b97cdb51c4d3028b646410661
hasira09 <88621706+hasira09@users.noreply.github.com>
2022-11-15 01:13:53 +0530

    Text and Icons

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel, MDIcon
 
 
-class TourCeylon(MDApp):
+class DemoApp(MDApp):
     def build(self):
-        return
+        label = MDLabel(text='Hello World', halign='center', theme_text_color='Custom',
+                        text_color=(236 / 255.0, 98 / 255.0, 81 / 255.0, 1),
+                        font_style='Caption')
+
+        icon_label = MDIcon(icon='library', halign='center')
+        return icon_label
 
 
-TourCeylon().run
+DemoApp().run()
