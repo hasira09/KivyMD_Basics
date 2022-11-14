250d92ae0d604f73dcb0dc1fba82496d3d992eb3
hasira09 <88621706+hasira09@users.noreply.github.com>
2022-11-15 01:44:31 +0530

    Themes

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton, MDFloatingActionButton
+from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.screen import Screen
 
 
-class DemoApp(MDApp):
+class BasicApp(MDApp):
 
     def build(self):
+        self.theme_cls.primary_palette = "DeepOrange"
+        self.theme_cls.primary_hue = "A700"
+        self.theme_cls.theme_style = "Dark"
         screen = Screen()
-        btn_flat = MDRectangleFlatButton(text='Hello World',
-                                         pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        btn_f = MDRectangleFlatButton(text='Hello World',
+                                      pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
-        icon_btn = MDFloatingActionButton(icon='language-python',
-                                          pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        screen.add_widget(icon_btn)
+        screen.add_widget(btn_f)
         return screen
 
 
-DemoApp().run()
+BasicApp().run()
