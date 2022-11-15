906bc0dcd42dc3cc8e9f67fe237de40478591529
hasira09 <88621706+hasira09@users.noreply.github.com>
2022-11-16 00:46:37 +0530

    Lists (Adding Icons)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.uix.scrollview import ScrollView
 from kivymd.app import MDApp
-from kivymd.uix.list import MDList, ThreeLineListItem
+from kivymd.uix.list import MDList, ThreeLineListItem, ThreeLineIconListItem
+from kivymd.uix.list import IconLeftWidget
 from kivymd.uix.screen import Screen
 
 
@@ -14,8 +15,11 @@ class TestApp(MDApp):
         scroll.add_widget(list_view)
 
         for i in range(20):
-            items = ThreeLineListItem(text='Item ' + str(i), secondary_text='KivyMD',
+            icons = IconLeftWidget(icon="android")
+            items = ThreeLineIconListItem(text='Item ' + str(i), secondary_text='KivyMD',
                                       tertiary_text='Hello World')
+
+            items.add_widget(icons)
             list_view.add_widget(items)
 
         screen.add_widget(scroll)
