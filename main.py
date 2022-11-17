319525e14a660474e034fab6575ada3379f40c1d
hasira09 <88621706+hasira09@users.noreply.github.com>
2022-11-17 13:02:03 +0530

    Navigation Drawer (I Part)

--- main.py
+++ main.py
@@ -4,41 +4,33 @@ from kivymd.app import MDApp
 
 Window.size = (300, 500)
 
-screen_helper = """
+navigation_helper = """
 Screen:
-    BoxLayout:
-        orientation: 'vertical'
+    MDNavigationLayout:
+        ScreenManager:
+            Screen:
+                BoxLayout:
+                    orientation: 'vertical'
         
-        #can use MDToolBar instead as well
-        MDTopAppBar:                
-            title: 'Demo App'
-            left_action_items: [["menu",lambda x: app.navigation_draw()]]
-            right_action_items: [["android",lambda x: app.navigation_draw()]]
-            elevation: 2
-            
-        MDLabel:
-            text: 'Hasira Mahel'
-            halign: 'center'
-            
-        MDBottomAppBar:
-            MDTopAppBar:
-                left_action_items: [["tea",lambda x: app.navigation_draw()]]
-                mode: 'end'
-                type: 'bottom'
-                icon: 'language-python'
-                on_action_button: app.navigation_draw()
-                elevation: 0
+                    #can use MDToolBar instead as well
+                    MDTopAppBar:                
+                        title: 'Demo App'
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state('toggle')]]
+                        elevation: 2
+                        
+                    Widget:
+                        
+        MDNavigationDrawer
+            id: nav_drawer
 """
 
 
 class NavApp(MDApp):
+
     def build(self):
         self.theme_cls.primary_palette = 'Purple'
-        screen = Builder.load_string(screen_helper)
+        screen = Builder.load_string(navigation_helper)
         return screen
-
-    def navigation_draw(self):
-        print("Navigation")
 
 
 NavApp().run()
